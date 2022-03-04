2261c9b2bc41f7c4bd5cad18ec00ee6a80f2d9b2
evanGRU <98941401+evanGRU@users.noreply.github.com>
2022-03-04 09:57:21 +0100

    Delete php directory

--- php/nav.php
+++
@@ -1,8 +0,0 @@
-    <header>
-        <nav>
-            <a href=<?php echo $index; ?>>ACCUEIL</a>
-            <a href=<?php echo $listing; ?>>SKINS</a>
-            <a href=<?php echo $admin; ?>>CONNEXION</a>
-            <a href=<?php echo $search; ?>>RECHERCHE</a>
-        </nav>
-    </header>
