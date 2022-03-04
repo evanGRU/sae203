8671d874eb8f87c6ff3488b55bdc4a4921d04ca6
evanGRU <evangruchot.pro@gmail.com>
2022-03-04 10:05:34 +0100

    commit1

--- php/connexion_bdd.php
+++ php/connexion_bdd.php
@@ -1,6 +1,6 @@
 <?php 
 
-    $mabd = new PDO('mysql:host=localhost;port=8888;dbname=mmi21d10;charset=UTF8;', 'root', 'root');
+    $mabd = new PDO('mysql:host=localhost;dbname=sae203;charset=UTF8;', 'sae203', 'devWeb10');
     $mabd->query('SET NAMES utf8;');
          
     $req ="SELECT * FROM skins INNER JOIN personnages ON skins._personnage_id=personnages.personnage_id"; 
